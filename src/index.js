ab3a9aeb6580923703ee70f9d460ed8e908cd133
Obraztsov Ivan <o6raztsv@gmail.com>
2021-01-16 00:23:08 +0300

    Исправлен index.js, добавлен env

--- src/index.js
+++ src/index.js
@@ -1,22 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import products from './products.json';
 
 import './index.css';
 
-const App = () => {
-  const pieceOfProducts = products.slice(0, 3);
-
-  return (
-    <>
-      <h1>Список товаров</h1>
-      <ul>
-        {pieceOfProducts.map(({id, name}) => (
-          <li key={id}>{name}</li>
-        ))}
-      </ul>
-    </>
-  );
-}
+import { App } from './components/App';
 
 ReactDOM.render(<App/>, document.getElementById('root'));
