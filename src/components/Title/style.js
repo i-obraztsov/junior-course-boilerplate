7a38565c340f620b5dc85d1cb3cbad69eb165002
Obraztsov Ivan <o6raztsv@gmail.com>
2021-01-16 23:21:14 +0300

    Добавлен компонент Title

---
+++ src/components/Title/style.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+  font-family: 'Open Sans', 'Helvetica Neue', 'Arial', sans-serif;
+  font-size: 36px;
+  line-height: 48px;
+  font-weight: 300;
+  text-align: center;
+  margin-top: 0;
+  margin-bottom: 56px;
+`;
