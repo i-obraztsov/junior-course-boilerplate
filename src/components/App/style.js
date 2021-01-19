9d76e96de009993e0e4121efff20bc18d555acdf
Obraztsov Ivan <o6raztsv@gmail.com>
2021-01-20 00:42:36 +0300

    компоненты переписаны на классы

--- src/components/App/style.js
+++ src/components/App/style.js
@@ -1,17 +1,23 @@
 import styled from 'styled-components';
 
-export const AppContent = styled.main`
-  align-items: flex-start;
-  display: flex;
-  flex-direction: row;
-  height: 100%;
-  padding: 20px;
-  width: 100%;
-`;
-
 export const AppContainer = styled.div`
   width: 100%;
   max-width: 768px;
   margin: 0 auto;
   margin-top: 60px;
+`;
+
+export const AppContent = styled.main`
+  position: relative;
+  height: 100%;
+  padding-top: 0;
+  width: 100%;
+`;
+
+export const Aside = styled.div`
+  width: 256px;
+  margin-right: 30px;
+  position: absolute;
+  top: 0;
+  right: 100%;
 `;
