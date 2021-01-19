6a2a22a94a8a3a9d8e520bcde6c9718c2932174d
Obraztsov Ivan <o6raztsv@gmail.com>
2021-01-20 00:37:04 +0300

    добавлены компоненты формы и filter

---
+++ src/components/Form/index.js
@@ -0,0 +1,13 @@
+import { Form } from './Form';
+import { Input } from './Input';
+
+import { Fieldset, Legend, Label, Button } from './style';
+
+export {
+  Form,
+  Input,
+  Fieldset,
+  Legend,
+  Label,
+  Button,
+}
