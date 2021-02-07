78503988d7e93b0e6a73465964ecd104fafa8a40
Obraztsov Ivan <o6raztsv@gmail.com>
2021-02-07 22:32:18 +0300

    переделаны компоненты формы

--- src/components/Form/Checkbox.js
+++ src/components/Form/Checkbox.js
@@ -1,25 +1,17 @@
 import React from 'react';
 import pt from 'prop-types';
-import { AppContext } from '../../AppContext';
 
 import { Checkbox as StyledCheckbox } from './style';
 
 export function Checkbox(props) {
   return (
-    <AppContext.Consumer>
-      {(context) => {
-        const { activeCategories } = context;
-        return (
-          <StyledCheckbox
-            type={props.type}
-            checked={activeCategories.includes(props.name)}
-            name={props.name}
-            id={props.id}
-            onChange={props.onChange}
-          />
-        )
-      }}
-    </AppContext.Consumer>
+    <StyledCheckbox
+      type={props.type}
+      checked={props.checked}
+      name={props.name}
+      id={props.id}
+      onChange={props.onChange}
+    />
   )
 }
 
