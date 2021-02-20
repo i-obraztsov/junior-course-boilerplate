e0e9c144151ddb4d36c38ede889dc8ec9c45be1b
Obraztsov Ivan <o6raztsv@gmail.com>
2021-02-20 03:59:03 +0300

    Переделан Filter
    Переделан Filter

--- src/components/Filter/index.js
+++ src/components/Filter/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import pt from 'prop-types';
 import { inputNumberMask } from '../../utils/inputNumberMask';
 import Discount from 'csssr-school-input-discount/lib';
+
+import { ResetLink, CategoryLink } from './style';
+
 import {
   Form,
   Input,
@@ -10,11 +13,7 @@ import {
   Label,
   InputWrap,
   Row,
-  Checkbox,
-  LabelAsButton
 } from '../Form';
-
-import { BaseButton } from '../../styles';
 
 export class Filter extends React.Component {
   static defaultProps = {
@@ -23,8 +22,7 @@ export class Filter extends React.Component {
     maxPrice: 10,
     discount: 0,
     categories: [],
-    activeCategories: [],
-    resetFilter: () => {},
+    activeCategory: '',
   }
 
   handleSubmit = (event) => {
@@ -42,128 +40,83 @@ export class Filter extends React.Component {
     setFilter(newState);
   }
 
-  handleChangeCategory = (event) => {
-    const { setFilter, activeCategories, pushHistory, setPage } = this.props;
-    let categories = [];
-
-    if (event.target.checked) {
-      categories = activeCategories.concat(event.target.name);
-    } else {
-      categories = activeCategories.filter(
-        (category) => category !== event.target.name
-      );
-    }
-
-    const newState = { activeCategories: categories };
-    pushHistory('category', {
-      category: categories,
-      page: 1,
-    });
-    setPage(1);
-    setFilter(newState);
-  }
-
-  handleResetFilter = (event) => {
-    event.preventDefault();
-
-    const { resetFilter, pushHistory } = this.props;
-
-    resetFilter();
-    pushHistory('category', {
-      category: null,
-    });
-  }
-
   render() {
     const {
-      activeCategories,
+      activeCategory,
       categories,
       discount,
       minPrice,
       maxPrice,
     } = this.props;
-
     return(
-      <Form method="post" action="#" onSubmit={this.handleSubmit}>
-        <Fieldset>
-          <Legend>Цена</Legend>
-          <Row noWrap>
-            <Label>
-              от
-              <InputWrap>
-                <Input
-                  name="minPrice"
-                  type="text"
-                  value={minPrice}
-                  onChange={this.handleChangePrice}
-                />
-              </InputWrap>
-            </Label>
+      <>
+        <Form method="post" action="#" onSubmit={this.handleSubmit}>
+          <Fieldset>
+            <Legend>Цена</Legend>
+            <Row noWrap>
+              <Label>
+                от
+                <InputWrap>
+                  <Input
+                    name="minPrice"
+                    type="text"
+                    value={minPrice}
+                    onChange={this.handleChangePrice}
+                  />
+                </InputWrap>
+              </Label>
 
-            <Label>
-              до
-              <InputWrap>
-                <Input
-                  name="maxPrice"
-                  type="text"
-                  value={maxPrice}
-                  onChange={this.handleChangePrice}
-                />
-              </InputWrap>
-            </Label>
-          </Row>
-        </Fieldset>
-
-        <Discount
-          title="Скидка"
-          name="discount"
-          onChange={this.handleChangePrice}
-          value={discount}
-        />
-
-        <Fieldset marginTop>
-          <Legend>Категории</Legend>
-            <Row>
-              {categories.map(category => {
-                return (
-                  <React.Fragment key={category}>
-                    <Checkbox
-                      id={category}
-                      type="checkbox"
-                      checked={activeCategories.includes(category)}
-                      name={category}
-                      onChange={this.handleChangeCategory}
-                    />
-                    <LabelAsButton as="label" htmlFor={category} secondary>
-                      {category}
-                    </LabelAsButton>
-                  </React.Fragment>
-                )
-              })}
+              <Label>
+                до
+                <InputWrap>
+                  <Input
+                    name="maxPrice"
+                    type="text"
+                    value={maxPrice}
+                    onChange={this.handleChangePrice}
+                  />
+                </InputWrap>
+              </Label>
             </Row>
-        </Fieldset>
+          </Fieldset>
 
-        <BaseButton
-          type="reset"
-          secondary
-          fullWidth
-          onClick={this.handleResetFilter}
-        >
-          Сбросить фильтры
-        </BaseButton>
-      </Form>
+          <Discount
+            title="Скидка"
+            name="discount"
+            onChange={this.handleChangePrice}
+            value={discount}
+          />
+        </Form>
+
+        <Fieldset as="div" marginTop>
+        <Legend as="div">Категории</Legend>
+          <Row>
+            {categories.map(category => (
+              <CategoryLink
+                key={category}
+                to={`?category=${category}`}
+                isActive={activeCategory === category}
+              >
+                {category}
+              </CategoryLink>
+            ))}
+          </Row>
+      </Fieldset>
+
+      <ResetLink to={{ search: '' }}>
+        Сбросить фильтры
+      </ResetLink>
+    </>
     )
   }
 }
 
 Filter.propTypes = {
   setFilter: pt.func.isRequired,
-  setPage: pt.func.isRequired,
   minPrice: pt.number.isRequired,
   maxPrice: pt.number.isRequired,
   discount: pt.number.isRequired,
   categories: pt.arrayOf(pt.string).isRequired,
-  activeCategories: pt.arrayOf(pt.string).isRequired,
-  resetFilter: pt.func.isRequired,
+  activeCategory: pt.string.isRequired,
 };
 
