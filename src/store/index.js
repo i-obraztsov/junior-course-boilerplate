30d9a72ca3e85ec50f44cdd2ec37b917394be5b6
Obraztsov Ivan <o6raztsv@gmail.com>
2021-02-20 03:50:34 +0300

    добавлены connect-router

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,15 @@
 import { createStore, combineReducers } from 'redux';
+import { connectRouter } from 'connected-react-router'
 import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 
+import { history } from '../utils/history';
+
 import filter from '../modules/filter';
-import pagination from '../modules/pagination';
 
 const reducer = combineReducers({
+  router: connectRouter(history),
   filter,
-  pagination,
-})
+});
 
 export const store = createStore(
   reducer,
